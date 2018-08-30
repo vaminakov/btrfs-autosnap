e9a5be9d2c6b985ce26f5d97d396963b59016fb9
VVL <me@ivvl.ru>
2018-08-30 10:47:27 +0300

    update to 1.0.0

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # Pacman hook which makes btrfs root snapshot before any action.
 ***
 ### Description: 
-This is a written on bash script and Pacman hook which makes btrfs root snapshot before any action. **Requires Archlinux-based OS (pacman), `btrfs-progs` and 'util-linux' to be installed.**
+This is a written on bash script with Pacman hook which help make btrfs root snapshots fast and simple. If your OS is Archlinux-based, Pacman hook will make snapshots before any change **Requires `btrfs-progs`, `util-linux` and `coreutils` to be installed. Also Archlinux-based OS for pacman hook**
 
-Feature is one and simple:
+Current features:
 * Hook will detect if your root filesystem is btrfs subvolume. If yes, it'll make snapshot of it.
-### Usage: 
+* Also you can manage snapshots: list and delete old ones. Use -h or --help key.
+### Installing: 
 ```
 1. Install btrfs-autosnap.sh to /usr/bin/btrfs-autosnap
-2. Install 01-btrfs-autosnap.hook to /usr/share/libalpm/hooks/01-btrfs-autosnap.hook
+2. Install btrfs-autosnap.conf to /etc/btrfs-autosnap.conf
+3. If your OS is Archlinux-based, install 01-btrfs-autosnap.hook to /usr/share/libalpm/hooks/01-btrfs-autosnap.hook
 Profit!
-
+```
 P.S. You can install it by [AUR package](https://aur.archlinux.org/packages/btrfs-autosnap/).
+### Usage: 
+```
+btrfs-autosnap [parameter] (only first parameter will be used!)
+Available commands:
+    -l,  --list           list existing snapshots with modify time,
+    -r,  --remove-old     remove old snapshots with config parameter,
+    -h,  --help           show this help and exit.
+    -v,  --version        show version information.
+If there are no parameter, btrfs-autosnap will delete old snapshots if necessary and make new one."
+```
--- btrfs-autosnap.sh
+++ btrfs-autosnap.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
-if [[ $(findmnt -n / | awk '{print $3}') != "btrfs" ]]
-then
-echo -e "\033[1;31mRoot filesystem is not btrfs! exiting...\033[0m"
-exit 1
-fi
+if [[ $EUID -ne 0 ]]; then sudo $0 $@ || su -c $0 $@ || exit 1; exit 0; fi
+source /etc/btrfs-autosnap.conf 2>/dev/null
+if [[ $(findmnt -n / | awk '{print $3}') != "btrfs" ]]; then echo -e "\033[1;31mRoot filesystem is not btrfs! exiting...\033[0m" && exit 1; fi
 subvol=$(findmnt -n / | awk '{print $4}' | grep -o subvol=/.* | sed 's/subvol=\///')
-if [[ -z ${subvol} ]]
-then
-echo -e "\033[1;31mRoot filesystem is not subvolume! exiting...\033[0m"
-exit 1
-fi
+if [[ -z ${subvol} ]]; then echo -e "\033[1;31mRoot filesystem is not subvolume! exiting...\033[0m" && exit 1; fi
+prepare() {
 mkdir -p /tmp/subvol
 mount $(findmnt -vn / | awk '{print $2}') /tmp/subvol
 cd /tmp/subvol
-for sub in $(find . -maxdepth 1 -type d -name "${subvol}-*" -printf "%T@ %f\n" | sort -nr | tail +2 | cut -d ' ' -f 2)
+}
+clean() {
+cd /tmp
+umount /tmp/subvol &>/dev/null
+}
+snap_delete() {
+for sub in $(find . -maxdepth 1 -type d -name "${subvol}-*" -printf "%T@ %f\n" | sort -nr | tail +$(($old+1)) | cut -d ' ' -f 2)
 do
-btrfs su delete $sub || echo -e "\033[1;31mError while deleting $sub!\033[0m"
+btrfs su delete $sub &>/dev/null && echo -e "\033[1;34mDeleted old subvol $sub\033[0m" || echo -e "\033[1;31mError while deleting old subvol $sub!\033[0m"
 done
-btrfs su snapshot ${subvol} ${subvol}-$(date +\%d.\%m.\%y) && echo -e "\033[1;32mCreated snapshot ${subvol}-$(date +\%d.\%m.\%y)!\033[0m" || echo -e "\033[1;31mError while creating snapshot ${subvol}-$(date +\%d.\%m.\%y)!\033[0m"
-umount /tmp/subvol
+if [[ -z "$sub" ]]; then echo -e "\033[1;34mThere are no old snapshots to be deleted!\033[0m"; fi
+}
+snap_make() {
+snap_delete
+new=$(</dev/urandom tr -dc "a-z0-9" | head -c6)
+btrfs su snapshot ${subvol} ${subvol}-$new &>/dev/null && echo -e "\033[1;32mCreated snapshot ${subvol}-$new!\033[0m" || echo -e "\033[1;31mError while creating snapshot ${subvol}-$new!\033[0m"
+}
+snap_list() {
+find . -maxdepth 1 -type d -name "${subvol}-*" -printf "%f\t%TY-%Tm-%Td %TH:%TM:%.2TS\n"
+}
+snap_help() {
+echo "Usage: btrfs-autosnap [parameter] (only first parameter will be used!)
+Available commands:
+    -l,  --list           list existing snapshots with modify time,
+    -r,  --remove-old     remove old snapshots with config parameter,
+    -h,  --help           show this help and exit.
+    -v,  --version        show version information.
+If there are no parameter, btrfs-autosnap will delete old snapshots if necessary and make new one."
+}
+snap_version() {
+echo "btrfs-autosnap by VVL
+version 1.0.0     30 august 2018"
+}
+case "${1}" in
+    -l|--list) prepare && snap_list && clean ;;
+    -r|--remove-old) prepare && snap_delete && clean ;;
+    -h|--help) snap_help ;;
+    -v|--version) snap_version ;;
+    *) prepare && snap_make && clean ;;
+esac
